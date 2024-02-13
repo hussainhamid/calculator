c1bd0478d7bb8087eebf7213e8e6306dae80320f
hussainhamid <143866624+hussainhamid@users.noreply.github.com>
2024-02-13 17:47:25 +0530

    updated numbers to display

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log("calculator");
+let display = document.querySelector("#display");
+let displayText = document.querySelector("#display-text");
+let numbers = document.querySelector("#numbers");
+//let operators = documnet.querySelector(".operator");
+let currentNum = "";
+let firstNum = "";
+let secondnum = "";
+let operator = "";
+
+function operate(a, operator, b) {
+    if (operator == "+") {
+        return add(a, b);
+    } else if (operator == "-") {
+        return subtract(a, b);
+    } else if (operator == "*") {
+        return multiply(a, b);
+    } else if (operator == "/") {
+        return divide(a, b);
+    }
+}
+
+//console.log(operate(2, "/", 2));
+
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+
+function populateDisplay(numbers) {
+    if (currentNum.length < 11) {
+        currentNum += numbers;
+    display.textContent = currentNum;
+    }
+};
+
+function clear(zero) {
+    display.textContent = zero;
+};
